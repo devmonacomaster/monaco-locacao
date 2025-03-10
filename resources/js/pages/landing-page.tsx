71ca1a9b6ba119d2832214cdeffd8738bf72824f
Raimundos Marques <devraimundos@outlook.com>
2025-03-10 10:29:19 -0300

    add: view page

---
+++ resources/js/pages/landing-page.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+    return (
+        <>
+            <h1 className="text-3xl">Monaco Locação</h1>
+        </>
+    );
+}
+
+export default LandingPage;
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return Inertia::render('landing-page');
+})->name('home'); // Define um alias para a rota, a qual pode ser chamada com interpolação de JS
+
+Route::get('/welcome', function () {
     return Inertia::render('welcome');
-})->name('home');
+})->name('welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +17,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
