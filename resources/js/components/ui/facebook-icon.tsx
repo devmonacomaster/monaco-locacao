4780afb6ce1aef501e29afd05d5666001ab93646
Raimundos Marques <devraimundos@outlook.com>
2025-03-13 13:16:33 -0300

    update

---
+++ resources/js/components/ui/facebook-icon.tsx
@@ -0,0 +1,9 @@
+function FacebookIcon() {
+  return (
+    <div className="flex items-center">
+        <img src="/images/facebook.svg" alt="Monaco" className="h-8 w-auto" />
+    </div>
+  );
+}
+
+export default FacebookIcon;
---
+++ resources/js/components/ui/monaco-icon.tsx
@@ -0,0 +1,9 @@
+function MonacoIcon() {
+    return (
+        <div className="flex items-center">
+            <img src="/images/monaco-logo.svg" alt="Monaco" className="h-8 w-auto" />
+        </div>
+    );
+}
+
+export default MonacoIcon;
