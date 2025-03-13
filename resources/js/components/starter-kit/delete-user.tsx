7a3eb146e7543dce20201688820ae17a98aa0aff
Raimundos Marques <devraimundos@outlook.com>
2025-03-13 14:39:39 -0300

    tradução

--- resources/js/components/starter-kit/delete-user.tsx
+++ resources/js/components/starter-kit/delete-user.tsx
@@ -35,24 +35,24 @@ export default function DeleteUser() {
             <HeadingSmall title="Delete account" description="Delete your account and all of its resources" />
             <div className="space-y-4 rounded-lg border border-red-100 bg-red-50 p-4 dark:border-red-200/10 dark:bg-red-700/10">
                 <div className="relative space-y-0.5 text-red-600 dark:text-red-100">
-                    <p className="font-medium">Warning</p>
-                    <p className="text-sm">Please proceed with caution, this cannot be undone.</p>
+                    <p className="font-medium">Alerta</p>
+                    <p className="text-sm">Por favor, proceda com cautela, essa ação não pode ser desfeita.</p>
                 </div>
 
                 <Dialog>
                     <DialogTrigger asChild>
-                        <Button variant="destructive">Delete account</Button>
+                        <Button variant="destructive">Deletar Conta</Button>
                     </DialogTrigger>
                     <DialogContent>
-                        <DialogTitle>Are you sure you want to delete your account?</DialogTitle>
+                        <DialogTitle>Tem certeza que você deseja deletar sua conta</DialogTitle>
                         <DialogDescription>
-                            Once your account is deleted, all of its resources and data will also be permanently deleted. Please enter your password
-                            to confirm you would like to permanently delete your account.
+                            Uma vez que sua conta for deletada, todos os seus dados também serão apagados. Insira sua senha
+                            para confirmar.
                         </DialogDescription>
                         <form className="space-y-6" onSubmit={deleteUser}>
                             <div className="grid gap-2">
                                 <Label htmlFor="password" className="sr-only">
-                                    Password
+                                    Senha
                                 </Label>
 
                                 <Input
@@ -72,12 +72,12 @@ export default function DeleteUser() {
                             <DialogFooter className="gap-2">
                                 <DialogClose asChild>
                                     <Button variant="secondary" onClick={closeModal}>
-                                        Cancel
+                                        Cancelar
                                     </Button>
                                 </DialogClose>
 
                                 <Button variant="destructive" disabled={processing} asChild>
-                                    <button type="submit">Delete account</button>
+                                    <button type="submit">Deletar Conta</button>
                                 </Button>
                             </DialogFooter>
                         </form>
