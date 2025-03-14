26daf398aabe5991edc3b10634453f5abcbf545c
Raimundos Marques <devraimundos@outlook.com>
2025-03-14 15:00:19 -0300

    update das imagens do banner

--- resources/js/pages/landing-page.tsx
+++ resources/js/pages/landing-page.tsx
@@ -2,9 +2,9 @@ import MonacoHeader from "@/components/monaco-header";
 import Carousel from "@/components/ui/monaco-carousel";
 
 const images = [
-    '/images/banner.webp',
-    '/images/banner.webp',
-    '/images/banner.webp',
+    '/images/banner-1.webp',
+    '/images/banner-2.webp',
+    '/images/banner-3.webp',
 ];
 
 function LandingPage() {
