cf03f658dbe46ede3388b0064ccda9c6120d33f3
Raimundos Marques <devraimundos@outlook.com>
2025-03-18 10:05:32 -0300

    update responsividade

--- resources/js/components/monaco-header.tsx
+++ resources/js/components/monaco-header.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 import FacebookLogo from '@/components/ui/icon-facebook';
 import InstagramLogo from '@/components/ui/icon-instagram';
 import MonacoLogo from '@/components/ui/icon-monaco-logo';
 import ArrowHeader from '@/components/ui/icon-arrow-header';
-import { Menu, X } from 'lucide-react';
 import './monaco-header.css';
 
 function MonacoHeader() {
@@ -17,18 +17,15 @@ function MonacoHeader() {
             </a>
 
             {/* Botão do menu mobile */}
-            <button
-                className="menu-button"
-                onClick={() => setIsOpen(!isOpen)}
-                aria-label="Abrir menu"
-            >
+            <button className="menu-button" onClick={() => setIsOpen(!isOpen)} aria-label="Abrir menu">
                 {isOpen ? <X className="icon" /> : <Menu className="icon" />}
             </button>
 
             {/* Menu mobile */}
             <nav className={`nav ${isOpen ? 'open' : ''}`}>
                 <ul className="nav-list">
-                    <div className='social-icons-div'>
+                    {/* Redes sociais - Links diretamente no componente */}
+                    <div className='social-icons-container'>
                         <li>
                             <a
                                 href="https://www.facebook.com/GrupoMonacoOficial/?locale=pt_BR"
@@ -50,22 +47,11 @@ function MonacoHeader() {
                             </a>
                         </li>
                     </div>
-                    <li>
-                        <a href={route('home')} className="nav-link">
-                            Nossos Carros
-                        </a>
-                    </li>
-                    <li>
-                        <a href={route('home')} className="nav-link">
-                            Sobre a Mônaco Locação
-                        </a>
-                    </li>
-                    <li>
-                        <a href={route('home')} className="cta">
-                            <span>RECEBA UMA PROPOSTA</span>
-                            <ArrowHeader className="arrow-icon" />
-                        </a>
-                    </li>
+                    {/* Links de navegação */}
+                    <li><a href={route('home')} className="nav-link">Nossos Carros</a></li>
+                    <li><a href={route('home')} className="nav-link">Sobre a Mônaco Locação</a></li>
+                    {/* Call to Action */}
+                    <li><a href={route('home')} className="cta"><span>RECEBA UMA PROPOSTA</span><ArrowHeader className="arrow-icon" /></a></li>
                 </ul>
             </nav>
         </header>
