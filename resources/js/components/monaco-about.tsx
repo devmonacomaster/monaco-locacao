8d7d18c3ac8e42575545b2e021dc81244136bb4c
Raimundos Marques <devraimundos@outlook.com>
2025-03-23 14:58:11 -0300

    update

--- resources/js/components/monaco-about.tsx
+++ resources/js/components/monaco-about.tsx
@@ -2,12 +2,44 @@ import "../../css/components/monaco-about.css";
 
 function AboutUs() {
     return (
-        <div className="container-about-us">
-            <div className="title-about-us">
+        <section className="container-about-us">
+            <header className="title-about-us">
                 <h1>Por que escolher a Mônaco Locação?</h1>
+            </header>
+
+            <img
+                src="/images/banner_justify.webp"
+                alt="Banner da Mônaco Locação"
+                className="image-about-us"
+            />
+
+            <div className="about-us-content">
+                <article className="section-about-us">
+                    <h2 className="about-us-title">Soluções premium</h2>
+                    <p className="about-us-text">
+                        Opções de locação de veículos de alto padrão para
+                        clientes que buscam desempenho e sofisticação.
+                    </p>
+                </article>
+
+                <article className="section-about-us">
+                    <h2 className="about-us-title">Abrangência nacional</h2>
+                    <p className="about-us-text">
+                        Centros de atendimento distribuídos em todo o país,
+                        garantindo suporte completo e acessível.
+                    </p>
+                </article>
+
+                <article className="section-about-us">
+                    <h2 className="about-us-title">Frota sob medida</h2>
+                    <p className="about-us-text">
+                        Variedade de veículos leves e pesados, adaptados às
+                        suas necessidades pessoais e profissionais, garantindo
+                        a melhor escolha para cada situação.
+                    </p>
+                </article>
             </div>
-            <img src="/images/banner_justify.webp" alt="Facebook Logo" className="image-about-us" />
-        </div>
+        </section>
     );
 }
 
